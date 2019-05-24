677fd868e6c523809e8cca077363b74b4a67f8ef
joelzhong <zhongbj_26210@163.com>
2019-05-24 13:55:08 +0800

    Update the logo.png.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
 	<div class="register">
 		<Row class="bg">
 			<Modal class="register-modal" v-model="registerModal" width="600" :closable="false" :mask-closable="false">
@@ -30,40 +29,6 @@
 						<Button type="primary" :loading="modal_loading" @click="" style="width: 150px">重置</Button>
 					</div>
 					<small style="text-align: center;display:block" class="subText">©copyright by HSblog</small>
-=======
-    <div class="register">
-        <Row class="bg">
-            <Modal class="register-modal" v-model="registerModal" width="600" :closable="false" :mask-closable="false">
-              <h2 class="tittle">
-				<Button type="Text" :loading="modal_loading" ><Icon type="chevron-left"></Icon>返回</Button>
-                <img class="cover" :src="require('@/assets/registerlogo.png')" alt="" height="50">
-              </h2>
-              <Form ref="formValidate" method="POST" action="/user/register" :model="formValidate" :rules="ruleValidate" :label-width="90">
-                <Form-item label="用户名 " prop="username">
-                  <Input v-model="formValidate.username" placeholder="请输入用户名" style="width: 400px"></Input>
-                </Form-item>
-                <Form-item label="密码 " prop="password">
-                  <Input v-model="formValidate.password" placeholder="请输入密码" style="width: 400px">
-                  </Input>
-                </Form-item>
-				<Form-item label="确认密码 " prop="confimpassword">
-                  <Input v-model="formValidate.confimpassword" placeholder="请再次输入密码" style="width: 400px">
-                  </Input>
-                </Form-item>
-				<Form-item label="手机号码 " prop="phone">
-                  <Input v-model="formValidate.phone" placeholder="请输入手机号码" style="width: 200px">
-                  </Input>
-                </Form-item>
-				<Form-item label="出生日期 " prop="birthday">
-                  <Input type="date" v-model="formValidate.birthday" placeholder="请输入出生日期" style="width: 150px">
-                  </Input>
-                </Form-item>
-              </Form>
-              <div slot="footer">
-				<div style="margin: 0 auto;display: block;width: 350px">
-					<Button type="success" :loading="modal_loading" @click="handleSubmit('formValidate')" style="width: 150px">注册</Button>
-					<Button type="primary" :loading="modal_loading"  style="width: 150px">重置</Button>
->>>>>>> 532f5349cb7b9ab70b3100a3c83461da8e13a268
 				</div>
 			</Modal>
 		</Row>
